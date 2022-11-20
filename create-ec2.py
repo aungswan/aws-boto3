8471ab9f6e6f7c7225b38850076a230d20c9994a
Ubuntu <ubuntu@ip-172-26-20-0.ap-southeast-1.compute.internal>
2022-11-20 14:09:22 +0000

    Add VPC and lunch EC2

--- create-ec2.py
+++ create-ec2.py
@@ -1,13 +1,15 @@
 import boto3
 myclient = boto3.client('ec2')
-myami = 'ami-0d43b5bf95246b21e'
+myami = 'ami-07651f0c4c315a529'
 
 vpc01 = '192.168.10.0/24'
-vpc01id = myclient.create_vpc(CidrBlock=vpc01)['Vpc']['VpcID']
+vpc01id = myclient.create_vpc(CidrBlock=vpc01)['Vpc']['VpcId']
 
-myclient.create_subnet(CidrBlock='192.168.10.10/16', VpcID=myvpcid)['Subnet']['SubnetId']
+myclient.create_subnet(CidrBlock='192.168.10.0/28', VpcId=vpc01id)
 
-myclient.run_instance(
+mysub = myclient.create_subnet(CidrBlock='192.168.10.16/28', VpcId=vpc01id)['Subnet']['SubnetId']
+
+myclient.run_instances(
     ImageId=myami,
     InstanceType='t2.micro',
     SubnetId=mysub,
