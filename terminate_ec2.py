208f3edcba8496f9efd8d6c0e522911b7e15f25e
Ubuntu <ubuntu@ip-172-26-20-0.ap-southeast-1.compute.internal>
2022-11-20 14:30:30 +0000

    Adding terminate ec2 related VPC01

---
+++ terminate_ec2.py
@@ -0,0 +1,15 @@
+import boto3
+import re
+client = boto3.client('ec2')
+response = client.describe_instances()
+
+ec2_list = []
+for a in response['Reservations']:
+    for b in a['Instances']:
+        regex = re.compile('(^192.168.10.*)', re.IGNORECASE)
+        if regex.match(b['PrivateIpAddress']):
+            ec2_list.append(b['InstanceId'])
+        
+        #print(b['InstanceId'],b['PrivateIpAddress'])
+
+print(client.terminate_instances(InstanceIds=(ec2_list)))
